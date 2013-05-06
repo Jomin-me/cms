f175df5783475ca6ffc0e722799421fdc51ba99b
Jomin <Jomin.me@gmail.com>
2013-05-06 08:54:21 +0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '3.2.13'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'sqlite3'
+end
+
+gem 'jquery-rails'
+gem 'execjs'
+gem 'therubyracer'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms::Application.config.secret_token = '74d1fa7996aeabb7671da327aeca1ae51d8085ccc316d077715a11b8acc65c96643072db0f15c7913c74a55592dbfde37066b62f1d4c05a7a4c7f66332714c86'
